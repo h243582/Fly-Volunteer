3e307e8a39805d855952cf850a6c54e80942ad1b
何昱飞 <2435823336@qq.com>
2021-11-07 23:22:13 +0800

    11.7

--- heyufei-Python/src/demo1.py
+++ heyufei-Python/src/demo1.py
@@ -2,7 +2,7 @@
 import requests
 
 # 2.确定url
-base_url = 'https://www.baidu.com/more/'
+base_url = 'https://gaokao.chsi.com.cn/'
 
 # 3.发送请求，获取响应
 response = requests.get(base_url)
@@ -17,7 +17,8 @@ response = requests.get(base_url)
 # response.encoding='utf-8'
 # print(response.text)
 
-#方法二：解码为utf-8
+# 方法二：解码为utf-8
+
 with open('index.html', 'w', encoding='utf-8') as fp:
     fp.write(response.content.decode('utf-8'))
 
