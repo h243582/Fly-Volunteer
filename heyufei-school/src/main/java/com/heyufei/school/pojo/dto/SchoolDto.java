29d1ba802068e8e9eea5e04a2b0f874982dbb59e
heyufei6 <Hh243582>
2022-02-21 15:51:17 +0800

    今天弄的院校库

--- heyufei-school/src/main/java/com/heyufei/school/pojo/dto/SchoolDto.java
+++ heyufei-school/src/main/java/com/heyufei/school/pojo/dto/SchoolDto.java
@@ -10,6 +10,17 @@ import javax.persistence.Table;
 public class SchoolDto {
 //    sc.id, sc.name, pr.name provinceName, de.name departmentName, type.name schoolType, le.name,
 //       top_university, sc.top_discipline, sc.graduate_school, sc.satisfaction
+select sc.id, sc.name, pr.name provinceName, de.name departmentName, type.name schoolType, le.name,
+    top_university, sc.top_discipline, sc.graduate_school, sc.satisfaction
+    from tb_school sc,tb_province pr,tb_department de,tb_type type,tb_levels le
+    where sc.tb_province_id = pr.id and sc.tb_department_id = de.id and sc.tb_type_id = type.id and sc.tb_levels_id = le.id
+
+    limit 1,50;
+
+
+
+
+
     private String
 
 }
